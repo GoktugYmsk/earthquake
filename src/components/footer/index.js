3ce2696067abd947ea3601946b310d04360f4e9a
GoktugYmsk <gogtug033@outlook.com>
2023-03-10 15:31:12 +0300

    Router added

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -3,12 +3,28 @@ import React, { useState } from 'react';
 import { FiRefreshCcw } from 'react-icons/fi';
 import { FaHome, FaStackExchange, FaMapMarkedAlt } from 'react-icons/fa';
 import 'bootstrap/dist/css/bootstrap.css';
+import { Routes, Route, NavLink } from 'react-router-dom';
 
 import Popup from '../popup/';
 import './index.css';
 
 
-function Lower({ refresh, boot,handleClick }) {
+function Statictic() {
+  return (
+    <div className='mainStatistic' >
+      Bu sayfa istatistik sayfasıdır
+    </div>
+  )
+}
+function Home() {
+  return (
+    <div>
+    </div>
+  )
+}
+
+
+function Lower({ refresh, boot, handleClick }) {
   const [showModal, setShowModal] = useState(false);
   const [showPopup, setShowPopup] = useState(false);
   const image4 = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRIr2rls4d-3sJHOzvVoNKuPh8gPWvjfezhCQ&usqp=CAU';
@@ -27,10 +43,12 @@ function Lower({ refresh, boot,handleClick }) {
     <div className="lowerBar">
       <nav>
         <ul>
-          <div className="home">
-            <FaHome className="homeIcon" size="35" color="black" />
-            <p>Anasayfa</p>
-          </div>
+          <NavLink to='/' >
+            <div className="home">
+              <FaHome className="homeIcon" size="35" color="black" />
+              <p className='mainPage' >Anasayfa</p>
+            </div>
+          </NavLink>
           <div onClick={handleRefreshClick} className="refresh">
             <FiRefreshCcw className="refreshIcon" size="35" color="black" />
             <p className="refreshParagraph">Yenile</p>
@@ -39,26 +57,25 @@ function Lower({ refresh, boot,handleClick }) {
             <img className="image4" src={image4} alt="icon" />
             <p className="glass">Filterele</p>
           </div>
-          <div onClick={boot} className="exchange">
-            <FaStackExchange className="exchangeIcon" size="35" color="black" />
-            <p className="statistik">İstatistik</p>
-          </div>
+          <NavLink to='/Statistic'>
+            <div onClick={boot} className="exchange">
+              <FaStackExchange className="exchangeIcon" size="35" color="black" />
+              <p className="statistik">İstatistik</p>
+            </div>
+          </NavLink>
           <div className="map">
             <FaMapMarkedAlt className="mapIcon" size="35" color="black" />
             <p className="mapParagraph">Harita</p>
           </div>
         </ul>
       </nav>
-      {/** component olacak */}
       {showPopup && (
         <Popup setShow={setShowPopup} />
       )}
-
-      {/*
-        <Routes>
-          <Route path="/Statistic" element={<Statictic />} />
-        </Routes>
-  */}
+      <Routes>
+        <Route path="/Statistic" element={<Statictic />} />
+        <Route path='/' element={<Home />} />
+      </Routes>
     </div>
   );
 }
--- src/components/popup/index.js
+++ src/components/popup/index.js
@@ -1,8 +1,11 @@
 import React from 'react'
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronDown } from '@fortawesome/free-solid-svg-icons'
+
 import './index.css'
 
-function Popup({setShow}) {
+function Popup({ setShow }) {
   const handleClosePopup = () => {
     setShow(false);
   };
@@ -12,8 +15,10 @@ function Popup({setShow}) {
       <div className="popupContent">
         <h1 className='search' >Arama</h1>
         <input className='footerInput' placeholder='Kelime ile filtrele' ></input>
+        <FontAwesomeIcon className='popupChevron' icon={faChevronDown} />
         <h1 className='filterHead' >İllere Göre Filtrele</h1>
         <input className='footerInputIl' placeholder='Tüm İller' ></input>
+        <FontAwesomeIcon className='popupChevronAlt' icon={faChevronDown} />
         <input className='footerInputS' placeholder='Sırala' ></input>
         <h1 className='arrangement' >Sıralama</h1>
         <button className='popupFilter' ><strong>Filtrele</strong></button>
